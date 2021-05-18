6d685683809d6b7247b2a69ab70a74ee9ee3a7bf
LashanaTello <lashana.tello@gmail.com>
2021-05-18 03:17:30 -0400

    fixed validation

--- backend/validation/bio.js
+++ backend/validation/bio.js
@@ -11,7 +11,7 @@ module.exports = function validateBioInput(data) {
     errors.bio = "Bio field can not be empty";
   }
 
-  if (!Validator.isLength(data.password, { min: 1, max: 450 })) {
+  if (!Validator.isLength(data.bio, { min: 1, max: 450 })) {
     errors.bio = "Bio must be at least 1 character and can not exceed 450 characters";
   }
 
