54520aaa008d4f8a80b16cc89f381fdf92081430
LashanaTello <lashana.tello@gmail.com>
2021-05-23 01:56:53 -0400

    Added redux-persist to fix issue where newly changed bio would not show after page refresh

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,18 +1,29 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import rootReducer from './reducers/rootReducer';
 
 const initialState = {};
 
 const middleware = [thunk];
 
+const persistConfig = {
+  key: 'root',
+  storage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 const store = createStore(
-  rootReducer,
+  persistedReducer,
   initialState,
   compose(
     applyMiddleware(...middleware)
     //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
+
+export const persistor = persistStore(store);
 
 export default store;
