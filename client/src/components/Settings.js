6c699bd1ba6dd7fab71ccb8ef750eb0a356d143b
LashanaTello <lashana.tello@gmail.com>
2021-06-11 03:04:18 -0400

    Added user info to display for settings page

--- client/src/components/Settings.js
+++ client/src/components/Settings.js
@@ -1,32 +1,204 @@
 import React, { Component } from 'react';
-import M from 'materialize-css';
+import { withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Settings extends Component {
   state = {
+    changeEmailShown: false,
+    changeUsernameShown: false,
+    changePasswordShown: false
   }
 
-  componentDidMount() {
-    M.Tabs.init(this.Tabs);
+  showEmailForm = () => {
+    this.setState({
+      changeEmailShown: !this.state.changeEmailShown
+    })
+  }
+
+  showUsernameForm = () => {
+    this.setState({
+      changeUsernameShown: !this.state.changeUsernameShown
+    })
+  }
+
+  showPasswordForm = () => {
+    this.setState({
+      changePasswordShown: !this.state.changePasswordShown
+    })
+  }
+
+  closeClicked = () => {
+    if (this.state.changeAvatarShown === true) {
+      this.setState({
+        changeAvatarShown: !this.state.changeAvatarShown
+      })
+    } else if (this.state.changeEmailShown === true) {
+      this.setState({
+        changeEmailShown: !this.state.changeEmailShown
+      })
+    } else if (this.state.changeUsernameShown === true) {
+      this.setState({
+        changeUsernameShown: !this.state.changeUsernameShown
+      })
+    } else {
+      this.setState({
+        changePasswordShown: !this.state.changePasswordShown
+      })
+    }
   }
 
   render() {
+    const { user } = this.props.auth;
+
     return (
       <div className="container">
         <div className="row">
           <div className="add-spacing col s12">
-            <ul ref={Tabs => { this.Tabs = Tabs; }} className="tabs">
-              <li key={"a"} className="tab col s6"><a href="#test1">Account Details</a></li>
-              <li key={"b"} className="tab col s6"><a href="#test2">Delete Account</a></li>
-            </ul>
+            Account Details
           </div>
-          <div id="test1" className="add-spacing col s12 center-align">
-            my details
-          </div>
-          <div id="test2" className="add-spacing col s12 center-align">Delete?</div>
         </div>
+        <div className="row">
+          <p className="col s3 right-align">Profile Picture</p>
+          <p className="dashboard-avatar col s5"><img src={user.avatar} alt="Your avatar" /></p>
+          <button className="btn col s2 move-down" style={{ display: "none"}} onClick={this.showAvatarForm}>Change Avatar</button>
+        </div>
+        <div className="row">
+          <div className="col s12">
+            <p className="col s3 right-align">Email Address:</p>
+            <p className="col s5">Your email</p>
+            <button className="btn col s2" onClick={this.showEmailForm}>Change Email</button>
+          </div>
+          <div className="col s12">
+            <p className="col s3 right-align">Username:</p>
+            <p className="col s5">{user.username}</p>
+            <button className="btn col s2" onClick={this.showUsernameForm}>Change Username</button>
+          </div>
+          <div className="col s12">
+            <p className="col s3 right-align">Password:</p>
+            <p className="col s5">*****</p>
+            <button className="btn col s2" onClick={this.showPasswordForm}>Change Password</button>
+          </div>
+
+        </div>
+        <div className="row">
+          <button className="col s2 push-s5 btn red center-align">Delete Account</button>
+        </div>
+
+        {
+          this.state.changeEmailShown && (
+            <div>
+              <div className="my-modal" style={{ height: "375px", width: "850px"}}>
+                <div className="row">
+                  <button className="btn-small right" onClick={this.closeClicked}><i className="material-icons">clear</i></button>
+                  <div className="col s12">
+                    <p className="col s3 offset-s1 right-align">Old Email:</p>
+                    <p className="col s6">
+                      <label>
+                        <input type="text" name="oldEmail" onChange={this.handleInputChange} required />
+                      </label>
+                    </p>
+                  </div>
+                  <div className="col s12">
+                    <p className="col s3 offset-s1 right-align">New Email:</p>
+                    <p className="col s6">
+                      <label>
+                        <input type="text" name="newEmail" onChange={this.handleInputChange} required />
+                      </label>
+                    </p>
+                  </div>
+                  <div className="col s12">
+                    <p className="col s3 offset-s1 right-align">Confirm New Email:</p>
+                    <p className="col s6">
+                      <label>
+                        <input type="text" name="newEmail2" onChange={this.handleInputChange} required />
+                      </label>
+                    </p>
+                  </div>
+                  <button className="col s2 offset-s5 btn center-align">Confirm</button>
+                </div>
+              </div>
+              <div className="dim-background"></div>
+            </div>
+          )
+        }
+        {
+          this.state.changeUsernameShown && (
+            <div>
+              <div className="my-modal" style={{ height: "300px"}}>
+                <div className="row">
+                  <button className="btn-small right" onClick={this.closeClicked}><i className="material-icons">clear</i></button>
+                  <div className="col s12">
+                    <p className="col s3 offset-s1 right-align">New Username:</p>
+                    <p className="col s6">
+                      <label>
+                        <input type="text" name="newUsername" onChange={this.handleInputChange} required />
+                      </label>
+                    </p>
+                  </div>
+                  <div className="col s12">
+                    <p className="col s3 offset-s1 right-align">Confirm New Username:</p>
+                    <p className="col s6">
+                      <label>
+                        <input type="text" name="newUsername2" onChange={this.handleInputChange} required />
+                      </label>
+                    </p>
+                  </div>
+                  <button className="col s2 offset-s5 btn center-align">Confirm</button>
+                </div>
+              </div>
+              <div className="dim-background"></div>
+            </div>
+          )
+        }
+        {
+          this.state.changePasswordShown && (
+            <div>
+              <div className="my-modal" style={{ height: "375px", width: "850px"}}>
+                <div className="row">
+                  <button className="btn-small right" onClick={this.closeClicked}><i className="material-icons">clear</i></button>
+                  <div className="col s12">
+                    <p className="col s3 offset-s1 right-align">Old Password:</p>
+                    <p className="col s6">
+                      <label>
+                        <input type="text" name="oldPassword" onChange={this.handleInputChange} required />
+                      </label>
+                    </p>
+                  </div>
+                  <div className="col s12">
+                    <p className="col s3 offset-s1 right-align">New Password:</p>
+                    <p className="col s6">
+                      <label>
+                        <input type="text" name="newPassword" onChange={this.handleInputChange} required />
+                      </label>
+                    </p>
+                  </div>
+                  <div className="col s12">
+                    <p className="col s3 offset-s1 right-align">Confirm New Password:</p>
+                    <p className="col s6">
+                      <label>
+                        <input type="text" name="newPassword2" onChange={this.handleInputChange} required />
+                      </label>
+                    </p>
+                  </div>
+                  <button className="col s2 offset-s5 btn center-align">Confirm</button>
+                </div>
+              </div>
+              <div className="dim-background"></div>
+            </div>
+          )
+        }
       </div>
     );
   }
 }
 
-export default Settings;
+Settings.propTypes = {
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps)(withRouter(Settings));
