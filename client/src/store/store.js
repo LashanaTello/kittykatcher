2896b8f926df98471165a6620adfda3c74466663
LashanaTello <lashana.tello@gmail.com>
2021-06-23 02:32:43 -0400

    Removed redux-persist

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,7 +1,5 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
 import rootReducer from './reducers/rootReducer';
 
 const initialState = {};
